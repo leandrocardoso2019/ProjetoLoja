6c908d852665aa0164e332997f3043fe38b1d07d
LEANDRO CARDOSO DE PANTAS JUNIOR <leandro.cpjunior@senacsp.edu.br>
2019-12-03 09:29:09 -0300

    Adição de novos itens ao menu

--- Loja/src/app/app.component.ts
+++ Loja/src/app/app.component.ts
@@ -20,6 +20,21 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Perfil',
+      url: '/perfil',
+      icon: 'person'
+    },
+    {
+      title: 'Meus Pedidos',
+      url: '/pedidos',
+      icon: 'basket'
+    },
+    {
+      title: 'Sair',
+      url: '/sair',
+      icon: 'exit'
     }
   ];
 
